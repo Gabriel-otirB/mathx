07d2d37b829a82d8b0754973b4d722429550961b
Gabriel-otirB <lordglayder@gmail.com>
2025-03-07 18:22:27 -0300

    Created controllers

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function home()
+    {
+        echo 'Apresentar a página inicial';
+    }
+
+    public function generateExercises(Request $request)
+    {
+        echo 'Gerar exercicios';
+    }
+
+    public function printExercises()
+    {
+        echo 'Imprimir exercícios no navegador';
+    }
+
+    public function exportExercises()
+    {
+        echo 'Exportar exercícios para um arquivo de texto';
+    }
+}
